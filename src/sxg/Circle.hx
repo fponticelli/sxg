6b1481026da0eb89f30157d645bb4f19b8111014
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-08 00:39:37 -0600

    added Circle

---
+++ src/sxg/Circle.hx
@@ -0,0 +1,19 @@
+package sxg;
+
+import sxg.core.Geom;
+import thx.Functions;
+import thx.geom.d2.*;
+import thx.geom.d2.Circle as C;
+import thx.math.Const;
+
+class Circle<T> extends Element<T> {
+  public var shape(default, null) : C;
+  function new(doc : sxg.core.Document<T>, cx : Float, cy : Float, radius : Float) {
+    super(doc, "circle");
+
+    this.shape = new C(
+      Geom.linkedPosition(doc, el, "cx", "cy", cx, cy),
+      Geom.linkedRadius(doc, el, radius)
+    );
+  }
+}
