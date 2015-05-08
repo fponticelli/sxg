93ff4f07db2140d14d603beecf2591f62d8fbc88
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-08 00:39:25 -0600

    completed Rect geometry

--- src/sxg/Rect.hx
+++ src/sxg/Rect.hx
@@ -1,16 +1,16 @@
 package sxg;
 
+import sxg.core.Geom;
 import thx.geom.d2.*;
 import thx.geom.d2.Rect as R;
 
 class Rect<T> extends Element<T> {
   public var shape(default, null) : R;
-  function new(doc : sxg.core.Document<T>) {
+  function new(doc : sxg.core.Document<T>, x : Float, y : Float, w : Float, h : Float) {
     super(doc, "rect");
-    /*
     this.shape = new R(
-
+      Geom.linkedPosition(doc, el, x, y),
+      Geom.linkedSize(doc, el, w, h)
     );
-    */
   }
 }
