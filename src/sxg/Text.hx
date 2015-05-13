2f6f8b79ea62e4190383d237f6b03f9c89cc5242
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-13 16:19:34 -0600

    moved anchor to text element

--- src/sxg/Text.hx
+++ src/sxg/Text.hx
@@ -8,6 +8,8 @@ class Text<T : {}> extends Element<T> {
   public var content(get, set) : String;
   public var position(default, null) : Point;
   public var textStyle(default, null) : TextStyle<T>;
+  public var anchor(get, set) : TextAnchor;
+
   function new(doc : Document<T>, x : Float, y : Float, content : String) {
     super(doc, "text");
     this.textStyle = new TextStyle(doc, el);
@@ -15,9 +17,35 @@ class Text<T : {}> extends Element<T> {
     Geom.linkedPosition(doc, el, x, y);
   }
 
-  function get_content() return doc.getTextContent(el);
-  function set_content(t : String) {
-    doc.setTextContent(el, t);
-    return t;
+  function get_anchor()
+    return TextAnchor.parse(doc.getAttribute(el, "text-anchor"));
+
+  function set_anchor(v : TextAnchor) {
+    doc.setAttribute(el, "text-anchor", v);
+    return v;
+  }
+
+  function get_content()
+    return doc.getTextContent(el);
+
+  function set_content(t : String)
+    return doc.setTextContent(el, t);
+}
+
+@:enum
+abstract TextAnchor(String) to String {
+  var Start = "start";
+  var Middle = "middle";
+  var End = "end";
+
+  public static function parse(s : String) : TextAnchor {
+    if(null == s)
+      return null;
+    return switch s.toLowerCase() {
+      case "start": Start;
+      case "end": End;
+      case "middle": Middle;
+      case _: throw 'Invalid anchor value "$s"';
+    };
   }
 }
--- src/sxg/TextStyle.hx
+++ src/sxg/TextStyle.hx
@@ -12,17 +12,8 @@ class TextStyle<T> {
     this.el = el;
   }
 
-  public var anchor(get, set) : TextAnchor;
   public var fontFamily(get, set) : String;
   public var fontSize(get, set) : Float;
-
-  function get_anchor()
-    return TextAnchor.parse(doc.getAttribute(el, "text-anchor"));
-
-  function set_anchor(v : TextAnchor) {
-    doc.setAttribute(el, "text-anchor", v);
-    return v;
-  }
 
   function get_fontFamily()
     return doc.getStyle(el, "font-family");
@@ -35,22 +26,4 @@ class TextStyle<T> {
 
   function set_fontSize(v : Float)
     return doc.setFloatStyle(el, "font-family", v);
-}
-
-@:enum
-abstract TextAnchor(String) to String {
-  var Start = "start";
-  var Middle = "middle";
-  var End = "end";
-
-  public static function parse(s : String) : TextAnchor {
-    if(null == s)
-      return null;
-    return switch s.toLowerCase() {
-      case "start": Start;
-      case "end": End;
-      case "middle": Middle;
-      case _: throw 'Invalid anchor value "$s"';
-    };
-  }
 }
